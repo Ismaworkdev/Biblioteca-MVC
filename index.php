e1ae478a638a813371f150d2736b2aef6dedb492
ismaworkdev <ismaelworkdv@gmail.com>
2024-12-30 16:36:38 +0100

    estructura

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblio-AR</title>
+    
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css">
+    <link rel="stylesheet" href="./css/estilos.css">
+    <script src="https://kit.fontawesome.com/f7fb471b65.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,300&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+   
+    <link rel="shortcut icon" href="favibook.png" type="image/x-icon">
+</head>
+<body>
+    <header class="header">
+        <nav class="navegacion">
+            <a class="anclaLogo" href="./index.html">
+               <img class="anclaLogo" src="./assets/img/logo.png" alt="">
+            </a>
+            <ul class="listaNav">
+                <li class="listaItem"><a class="linkNav iniciarSesion" href="./pages/registrarse.html">Registrarse</a></li>
+                <li class="listaItem"><a class="linkNav iniciarSesion" href="./pages/iniciosesion.html">Iniciar Sesión</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main id="main" class="main">
+        
+        <section data-aos="zoom-in"  data-aos-duration="1000" class="sectionPrincipal">
+            <h1 class="tituloPrincipal">Libros , Comic <br> y revistas </h1>
+            <h2 class="subtituloPrincipal">El estilo  que nesesites <br>para leer .</h2>
+            <a class="botonRegistrarse" href="./pages/registrarse.html">Registrate</a>
+        </section>
+        
+
+
+        <hr>
+        <section id="Disponibles" data-aos="fade-up" data-aos-offset="400" data-aos-delay="100" data-aos-duration="1000" class="librosDisponibles">
+            <h3 class="tituloSection">Disponibles</h3>
+            <div class="libros">
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                    
+                        <img class="imgDisponibles" src="./assets/img/peli_1.jpg" alt="El Hobbit" loading="lazy">
+                        
+                        <div class="tituloLibro">
+                            <h4>El Hobbit</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                    
+                        <img class="imgDisponibles" src="./assets/img/peli_2.jpg" alt="El Señor de los Anillos las Dos Torres" loading="lazy">
+                       
+                        <div class="tituloLibro">
+                            <h4>El Señor de los Anillos las Dos Torres</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                        
+                        <img class="imgDisponibles" src="./assets/img/peli_3.jpg" alt="El Señor de los Anillos la Comunidad del Anillo" loading="lazy">
+                        
+                        <div class="tituloLibro">
+                            <h4>El Señor de los Anillos la Comunidad del Anillo</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                        
+                        <img class="imgDisponibles" src="./assets/img/peli_4.jpg" alt="Harry Potter  y la camara secreta" loading="lazy">
+                        
+                        <div class="tituloLibro">
+                                <h4>Harry Potter y la Camara Secreta</h4>
+                        </div>
+                    </div>
+                   
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                        
+                        <img class="imgDisponibles" src="./assets/img/peli_5.jpg" alt="Harry Potter y el misterio del Principe" loading="lazy">
+                        
+                        <div class="tituloLibro">
+                            <h4>Harry Potter y el Misterio el Principe</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                        
+                        <img class="imgDisponibles" src="./assets/img/peli_6.jpg" alt="Harry Potter y la Piedra Filosofal" loading="lazy">
+                        
+                        <div class="tituloLibro">
+                            <h4>Harry Potter y la Piedra Filosofal </h4>
+                        </div>
+                        
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                        
+                        <img class="imgDisponibles" src="./assets/img/peli_7.jpg" alt="Los Juegos del Hambre" loading="lazy">
+                       
+                        <div class="tituloLibro">
+                            <h4>Los Juegos del Hambre</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                        
+                        <img class="imgDisponibles" src="./assets/img/peli_8.jpg" alt="Los Juegos del Hambre en LLamas" loading="lazy">
+                        
+                        <div class="tituloLibro">
+                            <h4>Los Juegos del Hambre en Llamas</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                        
+                        <img class="imgDisponibles" src="./assets/img/peli_9.jpg" alt="Los Juegos del Hambre Sinsajo" loading="lazy">
+                        
+                        <div class="tituloLibro">
+                            <h4>Los Juegos del Hambre Sinsajo</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                        
+                        <img class="imgDisponibles" src="./assets/img/peli_10.jpg" alt="Las Cronicas de Narnia" loading="lazy">
+                        
+                        <div class="tituloLibro">
+                            <h4>Las Cronicas Narnia</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                        
+                        <img class="imgDisponibles" src="./assets/img/peli_11.jpg" alt="Las Cronicas de Narnia,El Leon la Bruja y el Ropero" loading="lazy">
+                        
+                        <div class="tituloLibro">
+                            <h4>Las Cronicas de Narnia, el Leon la Bruja y el Ropero</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+                <a href="./pages/detalle.html">
+                    <div class="libro">
+                    
+                        <img class="imgDisponibles" src="./assets/img/peli_12.jpg" alt="Las Cronicas de Narnia, la ultima Batalla" loading="lazy">
+                       
+                        <div class="tituloLibro">
+                            <h4>Las Cronicas de Narnia, la Ultima Batalla</h4>
+                        </div>
+                    
+                    </div>
+                </a>
+            </div>
+       
+        </section>
+        <hr>
+
+    
+
+
+ 
+
+
+
+    </main>
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+    AOS.init();
+    </script>
+</body>
+</html>
