ba82717d82cd4b964b2cecdc000af0c8ab381a76
Ismael <ismaelworkdv@gmail.com>
2025-01-08 20:58:32 +0100

    session

--- vista/iniciosesion.php
+++ vista/iniciosesion.php
@@ -4,13 +4,15 @@ require_once './controller/Webontroller.php';
 $controller = new WebController();
 ?>
 
+
+
 <br><br>
 <section data-aos="zoom-in" data-aos-duration="1000" class="seccionRegistrarse">
     <form action="./index.php?action=login" method="POST">
         <h2 class="tituloRegistrarse">Iniciar Sesión</h2>
 
         <div>
-            <input type="text" placeholder="Usuario" name="usuariologin">
+            <input type="text" placeholder="Usuario" name="usuariologin" value="<?php echo isset($_POST['usuariologin']) ? $_POST['usuariologin'] : ''; ?>">
         </div>
 
         <?php $controller->mostrarerrores() ?>
--- vista/registrarse.php
+++ vista/registrarse.php
@@ -9,13 +9,13 @@ $controller = new WebController();
     <form action="./index.php?action=register" method="post">
         <h2 class="tituloRegistrarse">Registro</h2>
         <div>
-            <input type="text" placeholder="Nombre" id="nombre" name="nombre">
+            <input type="text" placeholder="Nombre" id="nombre" name="nombre" value="<?php echo isset($_POST['nombre']) ? $_POST['nombre'] : ''; ?>">
         </div>
         <div>
-            <input type="text" placeholder="Primer Apellido" id="apellido1" name="apellido1">
+            <input type="text" placeholder="Primer Apellido" id="apellido1" name="apellido1" value="<?php echo isset($_POST['apellido1']) ? $_POST['apellido1'] : ''; ?>">
         </div>
         <div>
-            <input type="text" placeholder="Segundo Apellido" id="apellido2" name="apellido2">
+            <input type="text" placeholder="Segundo Apellido" id="apellido2" name="apellido2" value="<?php echo isset($_POST['apellido2']) ? $_POST['apellido2'] : ''; ?>">
         </div>
 
 
